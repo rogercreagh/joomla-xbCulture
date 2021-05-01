f7f0d2909b2215d9a7e1d3cabc0c7887803bfd5a
rogercreagh <44980188+rogercreagh@users.noreply.github.com>
2021-05-01 08:34:56 +0100

    list wip

--- src/mod_xbculture_list/helper.php
+++ src/mod_xbculture_list/helper.php
@@ -2,7 +2,7 @@
 /*******
  * @package xbCulture
  * @filesource mod_xbculture_list/helper.php
- * @version 0.1.0 26th April 2021
+ * @version 0.1.0 1st May 2021
  * @author Roger C-O
  * @copyright Copyright (c) Roger Creagh-Osborne, 2021
  * @license GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
@@ -15,40 +15,67 @@ class modXbcultureListHelper {
 	
 	static function getItems($params) {
 		$cnt = $params->get('itemcnt');
-		$usebooks = Factory::getSession()->get('xbbooks_ok',false) && $params->get('usebooks');
-		$usefilms = Factory::getSession()->get('xbbooks_ok',false) && $params->get('usefilms');
-		$order = $params->get('itemcnt') ? 'rev_date' : 'cat_date';
+		$comp = $params->get('comp');
+//		$usebooks = Factory::getSession()->get('xbbooks_ok',false) && $params->get('usebooks');
+//		$usefilms = Factory::getSession()->get('xbbooks_ok',false) && $params->get('usefilms');
+		$filt = $params->get('filter');
+		$display = $params->get('display');
+		$sortby = $params->get('sortby');
+		$sortdir = $params->get('sortdir');
+		switch ($sortby) {
+			case 1:
+				$order = 'cat_date';
+				break;
+			case 2:
+				$order = 'rating';	
+				break;				
+			default:
+				$order = 'title';
+				break;
+		}
+		$img = ($comp=='xbbooks') ? 'cover_img' : 'poster_img';
 		$db = Factory::getDbo();
-		$films = array();
-		$books = array();
-		if ($usefilms) {
-			$query = $db->getQuery(true);
-			$query->select('f.id AS id, '.$order.' AS odate, f.title, f.poster_img AS image, "film" AS com')
-			->from('#__xbfilms AS f');
-			if ($order == 'rev_date') {
-				$query->join('INNER','#__xbfilmreviews AS r ON film_id = f.id');
-				$query->select('r.rating');
-			}
-			$query->order($order.' DESC');
-			$query->group('id');
-			$db->setQuery($query,0,$cnt);
-			$films = $db->loadObjectList();
+		$items = array();
+		$query = $db->getQuery(true);
+		$query->select('a.id AS id, a.cat_date, a.title, b.'.$img.' AS image')
+		->from('#_'.$comp.' AS a');
+		if ($sortby == 'rating') {
+			$itemid = ($comp=='xbbooks') ? 'book_id' : 'film_id';
+			$query->join('INNER','#_'.$comp.'reviews AS r ON '.$itemid.' = a.id');
 		}
-		if ($usebooks) {
-			$query = $db->getQuery(true);
-			$query->select('b.id AS id, '.$order.' AS odate, b.title, b.cover_img AS image, "book" AS com')
-			->from('#__xbbooks AS b');
-			if ($order == 'rev_date') {
-				$query->join('INNER','#__xbbookreviews AS r ON book_id = b.id');
-				$query->select('r.rating');
-			}
-			$query->order($order.' DESC');
-			$db->setQuery($query,0,$cnt);
-			$books = $db->loadObjectList();
+		$query->order($order.' '.$sortdir);
+		if ($order != 'title') {
+			$query->order($order.' ASC');
 		}
-		$items = array_merge($films,$books);
-		usort($items,'modXbcultureRecentHelper::dateSort');
+		$db->setQuery($query,0,$cnt);
+		$items = $db->loadObjectList();
 		return $items;
+		
+//		$books = array();
+// 		if ($usefilms) {
+// 			if ($order == 'rev_date') {
+// 				$query->join('INNER','#__xbfilmreviews AS r ON film_id = f.id');
+// 				$query->select('r.rating');
+// 			}
+// 			$query->order($order.' DESC');
+// 			$db->setQuery($query,0,$cnt);
+// 			$films = $db->loadObjectList();
+// 		}
+// 		if ($usebooks) {
+// 			$query = $db->getQuery(true);
+// 			$query->select('b.id AS id, '.$order.' AS odate, b.title, b.cover_img AS image, "book" AS com')
+// 			->from('#__xbbooks AS b');
+// 			if ($order == 'rev_date') {
+// 				$query->join('INNER','#__xbbookreviews AS r ON book_id = b.id');
+// 				$query->select('r.rating');
+// 			}
+// 			$query->order($order.' DESC');
+// 			$db->setQuery($query,0,$cnt);
+// 			$books = $db->loadObjectList();
+// 		}
+// 		$items = array_merge($films,$books);
+// 		usort($items,'modXbcultureRecentHelper::dateSort');
+// 		return $items;
 	}
 		
 	static function dateSort( $a, $b ) {
--- src/mod_xbculture_list/tmpl/default.php
+++ src/mod_xbculture_list/tmpl/default.php
@@ -2,7 +2,7 @@
 /*******
  * @package xbCulture
  * @filesource mod_xbculture_recent/tmpl/default.php
- * @version 0.1.0 26th April 2021
+ * @version 0.1.0 1st May 2021
  * @author Roger C-O
  * @copyright Copyright (c) Roger Creagh-Osborne, 2021
  * @license GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
@@ -12,7 +12,8 @@ defined( '_JEXEC' ) or die( 'Restricted access' );
 
 use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Language\Text;
-
+$comp = $params->get('comp');
+$view = $comp=='xbfilms' ? 'film' : 'book';
 ?>
 <div class="xb095 xbdarkgrey xbmb8">
 	<?php echo $params->get('pretext'); ?>
@@ -20,14 +21,14 @@ use Joomla\CMS\Language\Text;
 
 <ul style="list-style:none; margin-left:0;">
 <?php foreach ($items as $item) {
-  $link='index.php?option=com_xb'.$item->com.'s&view='.$item->com.'&id='.$item->id; ?>
-	<li><span class="icon-<?php echo ($item->com=='film'? 'screen xbfilm':'book xbbook');?>"></span>
+	$link='index.php?option=com_'.$comp.'&view='.$view.'&id='.$item->id; ?>
+	<li><span class="icon-<?php echo ($comp=='xbfilms'? 'screen xbfilm':'book xbbook');?>"></span>
     	<a href="<?php echo $link;?>"><?php echo $item->title;?></a>
     	<br /><span class="xbml20">&nbsp;
-    	<?php if ($params->get('reviewed')) : ?>
-        	<span class="xbhlt xbbold"><?php echo $item->rating;?></span><span class="icon-<?php echo ($item->rating==0 ? 'thumbs-down xbred':'star xbgold');?>"> </span>
+    	<?php if ($params->get('sortby')=='rating') : ?>
+        	<span class="xbhlt xbbold"><?php echo $item->rating;?></span> <span class="icon-<?php echo ($item->rating==0 ? 'thumbs-down xbred':'star xbgold');?>"> </span>
     	<?php endif; ?>
-    	<span class="xb09"><?php echo HtmlHelper::date($item->odate , Text::_('d M Y'));?></span></span></li>
+    	<span class="xb09"><?php echo HtmlHelper::date($item->cat_date , Text::_('d M Y'));?></span></span></li>
 <?php } ?>
 </ul>
 <div class="xb095 xbdarkgrey">
